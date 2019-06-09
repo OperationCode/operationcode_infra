49c3978913e4b54237cac030c3b81248bcfc9b6d
Kyle Holmberg <kylemh.email@gmail.com>
2019-06-08 17:51:17 -0700

    Format records and add TTL to client DNS records (#120)

--- dns/operationcode.org/records.tf
+++ dns/operationcode.org/records.tf
@@ -3,81 +3,83 @@ resource "dnsimple_record" "root" {
   name   = ""
   type   = "ALIAS"
   value  = "alias.zeit.co"
+  ttl    = 60
 }
 
 resource "dnsimple_record" "www" {
   domain = "${var.hosted-zone}"
-  name = "www"
-  type = "ALIAS"
-  value = "alias.zeit.co"
+  name   = "www"
+  type   = "ALIAS"
+  value  = "alias.zeit.co"
+  ttl    = 60
 }
 
 resource "dnsimple_record" "www_txt" {
   domain = "${var.hosted-zone}"
-  name = "_now"
-  type = "TXT"
-  value = "Qmd8XawRvuECtFLQm8SytbgcW2PV2jthtfMy7ujLnTN2gL"
+  name   = "_now"
+  type   = "TXT"
+  value  = "Qmd8XawRvuECtFLQm8SytbgcW2PV2jthtfMy7ujLnTN2gL"
 }
 
 resource "dnsimple_record" "api" {
   domain = "${var.hosted-zone}"
-  name = "api"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "api"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
 
 resource "dnsimple_record" "staging_api" {
   domain = "${var.hosted-zone}"
-  name = "api.staging"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "api.staging"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
 
 resource "dnsimple_record" "dashboards" {
   domain = "${var.hosted-zone}"
-  name = "dashboards"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "dashboards"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
 
 resource "dnsimple_record" "pybot" {
   domain = "${var.hosted-zone}"
-  name = "pybot"
-  type = "CNAME"
-  value = "${var.pybot-lb-ingress}"
+  name   = "pybot"
+  type   = "CNAME"
+  value  = "${var.pybot-lb-ingress}"
 }
 
 resource "dnsimple_record" "staging_pybot" {
   domain = "${var.hosted-zone}"
-  name = "pybot.staging"
-  type = "CNAME"
-  value = "${var.pybot-lb-ingress}"
+  name   = "pybot.staging"
+  type   = "CNAME"
+  value  = "${var.pybot-lb-ingress}"
 }
 
 resource "dnsimple_record" "staging_pybot_cert_verification" {
   domain = "${var.hosted-zone}"
-  name = "_69b5c7278c7c13092899e1b67e8de6c1.pybot.staging"
-  type = "CNAME"
-  value = "_9fdd906cbd86d545894523f8cd809812.ltfvzjuylp.acm-validations.aws"
+  name   = "_69b5c7278c7c13092899e1b67e8de6c1.pybot.staging"
+  type   = "CNAME"
+  value  = "_9fdd906cbd86d545894523f8cd809812.ltfvzjuylp.acm-validations.aws"
 }
 
 resource "dnsimple_record" "resources_api" {
   domain = "${var.hosted-zone}"
-  name = "resources"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "resources"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
 
 resource "dnsimple_record" "resources_staging_api" {
   domain = "${var.hosted-zone}"
-  name = "resources.staging"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "resources.staging"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
 
 resource "dnsimple_record" "python_backend_dev" {
   domain = "${var.hosted-zone}"
-  name = "api.dev"
-  type = "CNAME"
-  value = "${var.k8s-cluster-ingress}"
+  name   = "api.dev"
+  type   = "CNAME"
+  value  = "${var.k8s-cluster-ingress}"
 }
